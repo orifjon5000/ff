05e381cf7be94408189fe60e95d128af1450f72e
orifjon <orifjon.5000@gmail.com>
2022-05-13 01:35:56 +0900

    ItemaVErtical

--- src/component/Buyurtmalar/index.jsx
+++ src/component/Buyurtmalar/index.jsx
@@ -1,25 +1,69 @@
-import React,{useState} from "react";
-import { Container, Wrapper } from "./style";
+import React, { useState } from "react";
+import { Container, Wrapper,Order,Pay,User,Card,Location, Title } from "./style";
 import { card } from "../../mock/card";
 export const Index = () => {
-  const [products,setProducts]= useState(Object.entries(card))
+  const [products, setProducts] = useState(Object.entries(card));
   return (
     <Container>
       <Wrapper>
-        {products.map(([name,data])=>(
+        {products.map(([name, data]) => (
           <Wrapper.Column>
-           <h1>{name}</h1> 
-            {data.map((value)=>(
-              <div>
-              <h2>{value.user.name}</h2>
-              <h3>{value.user.phone}</h3>
-            </div>
+            <Title>
+              {" "}
+              <h4>{name}</h4>
+            </Title>
+            {data.map((value) => (
+              <Card>
+                <Order>
+                  {" "}
+                  <div>
+                    <p>{value.orderId}</p>
+                    <button>save</button>
+                  </div>
+                  <div>
+                    <button>clock</button>
+                    <p>00:24</p>
+                  </div>
+                </Order>
+
+                <User>
+                  <div>
+                  <button>user</button>
+                  <h4>{value.user.name}</h4>
+                  </div>
+                  <p> {value.user.phone}</p>
+                </User>
+
+
+                <Pay>
+                  <div>
+                    <p>Umumiy summa:</p>
+                    <p>{value.total} UZS</p>
+                  </div>
+                  <p>Payme</p>
+                </Pay>
+
+                <Location>
+                <div>
+                  <p>operator:</p>
+                  <h4>{value.operator.name}</h4>
+                </div>
+                <div>
+                  <p>Manzil:</p>
+                  <h4>{value.filial.location}</h4>
+                </div>
+                </Location>
+                <div>
+                  <button>s</button>
+                  <button>as</button>
+                </div>
+              </Card>
             ))}
           </Wrapper.Column>
-  ))} 
-        </Wrapper>
+        ))}
+      </Wrapper>
     </Container>
   );
 };
- 
+
 export default Index;
--- src/component/Buyurtmalar/style.js
+++ src/component/Buyurtmalar/style.js
@@ -1,20 +1,37 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div``;
 
-export const Wrapper =styled.div`
-display:flex;
+export const Wrapper = styled.div`
+  display: flex;
+`;
 
-`
+Wrapper.Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
 
-Wrapper.Column =styled.div`
-display:flex;
-flex-direction:column ;
-flex:1;
-height:100vh;
-background: whitesmoke;
-border-right:1px solid black;
-h1{
-    background:red;
-}
-`
+export const Card = styled.div`
+  width: 234px;
+  height: 345px;
+  background: #ffffff;
+  box-shadow: 0px 20px 25px rgba(176, 177, 181, 0.432802);
+  border-radius: 6px;
+  margin: 0 30px;
+  margin-bottom: 10px;
+  margin-right: 15px;
+`;
+
+export const Title = styled.div`
+  background: #ffffff;
+  box-shadow: 0px 2px 2px rgba(174, 176, 181, 0.314986);
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Order = styled.div``;
+export const User = styled.div``;
+export const Pay = styled.div``;
+export const Location = styled.div``;
