fff55a43d1b31c6bf2fb04161e646b8dba95a789
orifjon <orifjon.5000@gmail.com>
2022-05-13 17:35:22 +0900

    productitems

--- src/component/Buyurtmalar/index.jsx
+++ src/component/Buyurtmalar/index.jsx
@@ -1,5 +1,20 @@
 import React, { useState } from "react";
-import { Container, Wrapper,Order,Pay,User,Card,Location, Title } from "./style";
+import Save from "../../assets/icons/save.svg";
+import Clock from "../../assets/icons/clock.svg";
+import UserI from "../../assets/icons/user.svg";
+import Done from "../../assets/icons/done.svg";
+import Cancel from "../../assets/icons/cancel.svg";
+
+import {
+  Container,
+  Wrapper,
+  Order,
+  Pay,
+  User,
+  Card,
+  Location,
+  Title,
+} from "./style";
 import { card } from "../../mock/card";
 export const Index = () => {
   const [products, setProducts] = useState(Object.entries(card));
@@ -14,49 +29,54 @@ export const Index = () => {
             </Title>
             {data.map((value) => (
               <Card>
-                <Order>
+                <Order className="flex-row">
                   {" "}
-                  <div>
-                    <p>{value.orderId}</p>
-                    <button>save</button>
+                  <div className="flex-row  orderId">
+                    <p className="flex-row">{value.orderId}</p>
+                    <div className="imgsave">
+                      <img src={Save} alt="save" />
+                    </div>{" "}
                   </div>
-                  <div>
-                    <button>clock</button>
+                  <div className="flex-row orderClock">
+                    <img src={Clock} alt="Clock" />
                     <p>00:24</p>
                   </div>
                 </Order>
 
-                <User>
-                  <div>
-                  <button>user</button>
-                  <h4>{value.user.name}</h4>
+                <User className="flex-column">
+                  <div className="flex-row userName">
+                    <img src={UserI} alt="User" />
+                    <h4>{value.user.name}</h4>
                   </div>
                   <p> {value.user.phone}</p>
                 </User>
 
-
-                <Pay>
-                  <div>
+                <Pay className="flex-row">
+                  <div className="payTotal">
                     <p>Umumiy summa:</p>
                     <p>{value.total} UZS</p>
                   </div>
-                  <p>Payme</p>
+                  <div className="iconpay"></div>
+                  <span>Payme</span>
                 </Pay>
 
-                <Location>
-                <div>
-                  <p>operator:</p>
-                  <h4>{value.operator.name}</h4>
-                </div>
-                <div>
-                  <p>Manzil:</p>
-                  <h4>{value.filial.location}</h4>
+                <Location className="flex-row">
+                  <div>
+                    <div>
+                      <p>operator:</p>
+                      <h4>{value.operator.name}</h4>
+                    </div>
+                    <div>
+                      <p>Manzil:</p>
+                      <h4>{value.filial.location}</h4>
+                    </div>
+                  </div>
+                  <div className=" flex-column">
+                  <img className="imgSave" src={Done} alt="Done" />
+                  <img src={Cancel} className="imgSave" alt="Cancel" />
                 </div>
                 </Location>
-                <div>
-                  <button>s</button>
-                  <button>as</button>
-                </div>
+               
               </Card>
             ))}
           </Wrapper.Column>
