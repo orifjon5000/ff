ec339f53d7d78de0e3bab80c8c1de109711919d6
orifjon <orifjon.5000@gmail.com>
2022-05-24 15:42:44 +0900

    googlemap key

--- src/component/Filial/index.jsx
+++ src/component/Filial/index.jsx
@@ -3,21 +3,21 @@ import { Container } from "./style";
 import { useLocation } from "react-router-dom";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
-const key = "AIzaSyAgPyjTkq0DrZk0eLdlVtxlyxQjb56-glw";
-
+const key = process.env.REACT_APP_API_KEY;
+console.log(key)
 function Index() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: key,
     id: "main",
   });
   return (
-    <Container>
+    <Container className="container">
       {isLoaded && (
         <GoogleMap
           id="main"
           mapContainerStyle={{ width: "1000px", height: "600px" }}
-          zoom={4}
-          center={{ lat: 41.311081, lang: 69.240562 }}
+          zoom={15}
+          center={{ lat: 40.11583, lng:  67.84222 }}
         ></GoogleMap>
       )}
     </Container>
