2589b27bf39553955bcca16478e3f2c744ec9576
orifjon <orifjon.5000@gmail.com>
2022-05-24 16:53:37 +0900

    map

--- src/component/Filial/index.jsx
+++ src/component/Filial/index.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { Container } from "./style";
-import { useLocation } from "react-router-dom";
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { Autocomplete, GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const key = process.env.REACT_APP_API_KEY;
-console.log(key)
+console.log(key);
+const libraries = ["places"];
 function Index() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: key,
     id: "main",
+    libraries,
   });
   return (
     <Container className="container">
+      {/* <Autocomplete>
+        <input type="text" />
+      </Autocomplete> */}
       {isLoaded && (
         <GoogleMap
           id="main"
           mapContainerStyle={{ width: "1000px", height: "600px" }}
           zoom={15}
-          center={{ lat: 40.11583, lng:  67.84222 }}
-        ></GoogleMap>
+          center={{ lat: 40.11583, lng: 67.84222 }}
+        >
+          <Marker position={{ lat: 40.11583, lng: 67.84222 }} />
+        </GoogleMap>
       )}
     </Container>
   );
